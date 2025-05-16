1e1932fd15f290cbf6a3c71174b3979e608895c0
Sergio <smoreno@birt.eus>
2025-05-16 10:36:49 +0200

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\CategoryGatewayController;
 use App\Http\Controllers\ProductGatewayController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/products/min', [ProductGatewayController::class . 'stockMin']);
+Route::get('/products/min', [ProductGatewayController::class, 'stockMin']);
 Route::apiResource('products', ProductGatewayController::class);
 Route::apiResource('categories', CategoryGatewayController::class);
